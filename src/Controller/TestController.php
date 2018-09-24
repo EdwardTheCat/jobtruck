4cd091965e2a155c11d544efc118dfdd74b993f1
Kévin <johndoe@example.com>
2018-09-24 09:54:48 +0200

    ajout templates base

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -12,6 +12,24 @@ class TestController extends AbstractController
      */
     public function index()
     {
-        return $this->render('testBase.html.twig');
+        /* return $this->render('testBase.html.twig'); */
+        return $this->render('testPageConcept.html.twig');
     }
+
+    /**
+    * @Route("/test/concept", name="conceptPage")
+    */
+    public function showConceptPage()
+    {
+        return $this->render('conceptPage.html.twig');
+    }
+
+    /**
+    * @Route("/test/temoignages", name="testimonyPage")
+    */
+    public function showTestimonyPage()
+    {
+        return $this->render('testimonyPage.html.twig');
+    }
+
 }
